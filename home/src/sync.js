3973de5e0cc30b79749b15cdc9e7de7ddedaf031
Dmytri Kleiner <dk@trick.ca>
2022-05-21 22:46:16 +0200

    ...

--- home/src/sync.js
+++ home/src/sync.js
@@ -1,3 +1,13 @@
-console.log('SYNC SITES NOW')
-console.log('username:',process.env.GIT_USERNAME)
-console.log('password:',process.env.GIT_PASSWORD)
+const debug = require('debug')('sync-sites')
+
+const GIT_HOST='http://onedev:6610'
+const SITES_ROOT='/usr/share/nginx/html/sites'
+
+const gitPullOrClone = require('git-pull-or-clone')
+gitPullOrClone(`${GIT_HOST}/test`,
+  `${SITES_ROOT}/test`,
+  (err) => {
+    if (err) throw err
+    debug('Success!')
+  }
+)
